8623fdb1a2bc356e15561984ab9853c325106d34
Olga Shkola <shkola.olga@gmail.com>
2026-01-03 16:09:51 +0300

    Deployed 0f587a4 with MkDocs version: 1.6.1

--- storage/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Модуль для работы с различными типами хранилищ
-"""
-from .yandex_disk import YandexDiskStorage
-from .local_storage import LocalStorage
-from .hybrid_storage import HybridStorage, StorageMode
-
-__all__ = ['YandexDiskStorage', 'LocalStorage', 'HybridStorage', 'StorageMode']
